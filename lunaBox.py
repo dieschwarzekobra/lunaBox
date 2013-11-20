4fe8f32a05e43c8b2a3e0afde6549629975d5bb6
Jaleesa Powell <jaleesap@email.unc.edu>
2013-11-20 09:25:43 -0500

    #4 . Updated lunaBox.py

--- lunaBox.py
+++ lunaBox.py
@@ -3,6 +3,8 @@ import sqlite3
 # Define and initialize global variables
 numEpisodesPrinted = 0
 count = 0
+episodeURLs = []
+PASSED_URL = ''
 STATEMENT_PRINT_ALL_TITLES = 'SELECT * FROM sailorMoon'
 STATEMENT_PAGINATE = 'SELECT * FROM sailorMoon WHERE episode > ' + str(numEpisodesPrinted) + ' ORDER BY episode LIMIT 3'
 
@@ -15,7 +17,6 @@ def countRows(statement):
   for row in c.execute(statement):
     global count
     count += 1
-  global count
   return count
 
 # Main Loop for the program
@@ -29,16 +30,29 @@ def runProgram():
 # Print limited number of results
 def printEpisodes():
   for row in c.execute(STATEMENT_PAGINATE):
+    # Update the count of the number of episodes printed
     global numEpisodesPrinted
     numEpisodesPrinted += 1
-    print row[1] + ". Num Printed: " + str(numEpisodesPrinted)
 
-#Wait for user input before continuing to print results
+    # Add URL of the current episode to the array of callable URLs / episodes
+    episodeURLs.append(row[0])
+
+    # Print the title and number of the episode
+    print str(numEpisodesPrinted) + ". " + row[1]
+
+#Wait for user input before continuing to print results or URL
 def paginate():
-  userInput = raw_input("Continue?: ")
+  userInput = raw_input("Enter yes your episode number of choice to continue: ")
   if userInput == "Yes":
     printEpisodes()
+  else:
+    global episodeURLs
+    global PASSED_URL
+    PASSED_URL = episodeURLs[int(userInput)-1]
+    printSelectedURL()
 
-
+#Print the selected episodeURL
+def printSelectedURL():
+  print PASSED_URL
 
 runProgram()
