050db283c59fb5ba2e91e392f34c0f25224c3c54
Jaleesa Powell <jaleesap@email.unc.edu>
2013-11-19 21:41:14 -0500

    Print six titles at once

--- lunaBox.py
+++ lunaBox.py
@@ -1,7 +1,8 @@
 import sqlite3
 
 # Define and initialize global variables
-STATEMENT = 'SELECT * FROM sailorMoon ORDER BY episode'
+STATEMENT_PRINT_ALL_TITLES = 'SELECT * FROM sailorMoon ORDER BY episode'
+STATEMENT_PAGINATE = 'SELECT * FROM sailorMoon ORDER BY episode LIMIT 6'
 
 # Define a querying function that prints to the console
 def query(statement, i):
@@ -15,10 +16,17 @@ def query(statement, i):
       print row
   return
 
+# Count the number of records in the database
+def count(statement):
+  rows = c.execute(statement)
+
+
 # Connect to the database
 conn = sqlite3.connect('sailorMoon.sqlite')
 c = conn.cursor()
 
 # Query the database and print titles
-query(STATEMENT, 1)
+# query(STATEMENT, 1)
 
+# Query the database and print six entries
+query(STATEMENT_PAGINATE, 1)
